83d91ba742070815c6de178c72f5cc73ec46d3b6
Flavio-Portela <fmspx@hotmail.com>
2023-03-04 14:43:51 -0300

    master: no message

--- lib/util-call.sh
+++ lib/util-call.sh
@@ -10,11 +10,6 @@ function __prepareEnvs()
     return 0;
   fi
 
-  if [[ ${QT_DIR} == "" ]] ; then
-    echo "invalid env: QT_DIR"
-    return 0;
-  fi
-
   if [[ ${QT_VERSION} == "" ]] ; then
     echo "invalid env: QT_VERSION"
     return 0;
@@ -24,6 +19,12 @@ function __prepareEnvs()
     export PATH=${PATH}:${STACK_INSTALLER_DIR}
   fi
 
+  export QT_QPA_PLATFORM=offscreen
+  export QT_ACCESSIBILITY=1
+  export QT_AUTO_SCREEN_SCALE_FACTOR=0
+  export QT_REFORCE_LOG=true
+
+  export QT_DIR=${PUBLIC_LIB_DIR}/qt
   export QT_LIBRARY_PATH=${QT_DIR}/${QT_VERSION}
   export LD_LIBRARY_PATH=${QT_LIBRARY_PATH}/lib
   export QT_PLUGIN_PATH=${QT_LIBRARY_PATH}/plugins
