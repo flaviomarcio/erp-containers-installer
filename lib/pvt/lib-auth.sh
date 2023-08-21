19b4c8a076773d8bbdd55ed364f826e39fb558b1
Flavio-Portela <fmspx@hotmail.com>
2023-08-20 22:36:39 -0300

    feature/users: no message

--- lib/pvt/lib-auth.sh
+++ lib/pvt/lib-auth.sh
@@ -21,7 +21,7 @@ export CMD_FILE=/tmp/req.sh
 
 function loadCredential()
 {
-  export AUTH_HOST=${STACK_ENVIRONMENT}-${STACK_TARGET}-srv-auth  
+  export AUTH_HOST=${STACK_ENVIRONMENT}-${STACK_TARGET}-srv-auth:8080
   export AUTH_CONTEXT_PATH=
   export AUTH_URI="http://${AUTH_HOST}${AUTH_CONTEXT_PATH}/api"
   export CLIENT_ID=${STACK_SERVICE_DEFAULT_USER}
