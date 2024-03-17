6e69bb76c3ef17f3b072e25569faac247c000433
Flavio-Portela <fmspx@hotmail.com>
2024-03-16 21:52:28 -0300

    [release/1.0.0] news changes

--- runner.sh
+++ runner.sh
@@ -20,12 +20,106 @@ export INSTALLER_LIB=${INSTALLER_DIR}/lib
 . ${BASH_BIN}/lib-database.sh
 . ${INSTALLER_LIB}/util-runner.sh
 
+function __applicationVerify()
+{
+  if [[ $(which jq) == "" || $(which yq) == "" ]]; then
+    echR "Application not found: jq"
+    echB "  install applications to continue"
+    echo ""
+    echY "    > sudo apt install -y jq yq"
+    echo ""
+    return 0
+  fi
 
-function __private_runnerMenu()
+  return 1
+}
+
+function __private_check_is_test()
+{
+  for __arg in "$@"
+  do
+    if [[ ${__arg} == "--test" ]]; then
+      return 1;
+    fi
+  done
+  return 0;
+}
+
+function __private_check_is_pipeline()
+{
+  for __arg in "$@"
+  do
+    if [[ ${__arg} == "--pipeline" ]]; then
+      return 1;
+    fi
+  done
+  return 0;
+}
+
+function __runnerPrepare()
 {
   clearTerm
+  __applicationVerify
+  if ! [ "$?" -eq 1 ]; then
+    echR "  fail on calling __applicationVerify"
+    return 0
+  fi
+  utilInitialize "$@"
+  dockerSwarmVerify
+  if ! [ "$?" -eq 1 ]; then
+    echR "  fail on calling dockerSwarmVerify"
+    return 0
+  fi
+
+  if [[ ${__public_target} == "" ]]; then
+    selectorCustomer 1
+    if ! [ "$?" -eq 1 ]; then
+      echR "Invalid selectorCustomer"
+      echR "  fail on calling selectorCustomer"
+      return 0
+    fi
+    export __public_target=${__selector}
+  fi
+
+  if [[ ${__public_environment} == "" ]]; then
+    selectorEnvironment 1
+    if ! [ "$?" -eq 1 ]; then
+      echR "  fail on calling selectorEnvironment"
+      return 0
+    fi
+    export __public_environment=${__selector}
+  fi
+
+  utilPrepareInit "${__public_environment}" "${__public_target}"
+
+  if ! [ "$?" -eq 1 ]; then
+    echR "  fail on calling utilPrepareInit"
+    return 0
+  fi
+
+  databasePrepare ${__public_environment} ${STACK_APPLICATIONS_DATA_DB_DIR}
+  if ! [ "$?" -eq 1 ]; then
+    echR "  fail on calling databasePrepare"
+    return 0
+  fi
+
+  scriptsPrepare ${__public_environment} ${STACK_APPLICATIONS_DATA_SCRIPT_DIR}
+  if ! [ "$?" -eq 1 ]; then
+    echR "  fail on calling scriptsPrepare"
+    return 0
+  fi
+  return 1
+}
+
+function __private_runnerMenuOptions()
+{
+  __runnerPrepare
+  if ! [ "$?" -eq 1 ]; then
+    echR "fail on call __runnerPrepare"
+    return 0
+  fi
   __private_print_os_information
-  echC "  - Docker Dir: ${COLOR_BLUE_B}${COLOR_YELLOW}${STACK_INSTALLER_DOCKER_DIR}"
+  echC "  - Application Dir: ${COLOR_YELLOW}${STACK_APPLICATIONS_DIR}"
   __runner_menu_environment=${1} 
   __runner_menu_target=${2}
 
@@ -34,7 +128,7 @@ function __private_runnerMenu()
   options+=(Docker-Build-SRV)
   options+=(Docker-Build-MCS)
   options+=(Docker-Build-ADM)
-  options+=(Vault)
+  options+=(Vault-options)
   options+=(Database-Update)
   #options+=(Database-DDL-Maker)
   #options+=(Database-PGPass)
@@ -60,8 +154,8 @@ function __private_runnerMenu()
       dockerMCSMain ${__runner_menu_environment} ${__runner_menu_target}
     elif [[ ${opt} == "Docker-Build-ADM" ]]; then
       dockerADMMain ${__runner_menu_environment} ${__runner_menu_target}
-    elif [[ ${opt} == "Vault" ]]; then
-      vaultMain ${__runner_menu_environment} ${__runner_menu_target}
+    elif [[ ${opt} == "Vault-options" ]]; then
+       vaultMain ${__runner_menu_environment} ${__runner_menu_target}
     elif [[ ${opt} == "Database-Update" ]]; then
       databaseUpdateMain ${__runner_menu_environment} ${__runner_menu_target}
     elif [[ ${opt} == "Database-DDL-Maker" ]]; then
@@ -87,68 +181,102 @@ function __private_runnerMenu()
   done
 }
 
+function __private_runnerMenu()
+{
+  local __public_environment=${1}
+  local __public_target=${2}
+  while :
+  do
+    __private_runnerMenuOptions ${__public_environment} ${__public_target}
+  done
+}
+
+
+function __private_pipeline_runner()
+{  
+  unset PIPELINE_ENVIRONMENT
+  unset PIPELINE_NAME
+  unset PIPELINE_ENV_FILE
+
+  unset __pipe_arg_key
+  unset __pipe_arg_val
+
+  local __pipe_arg_key_list=()
+
+  for __pipe_arg in "$@"
+  do
+    unset __pipe_arg_key
+    unset __pipe_arg_val
+    if [[ ${__pipe_arg} != --pipeline-* ]]; then
+      continue;
+    fi
+    local __pipe_arg_key=$(echo ${__pipe_arg} | awk -F= '{print $1}')
+    local __pipe_arg_val=$(echo ${__pipe_arg} | awk -F= '{print $2}')
+
+    local __pipe_arg_key=$(echo ${__pipe_arg_key} | sed 's/--//g' | sed 's/-/_/g')
+    local __pipe_arg_key=$(toUpper "${__pipe_arg_key}")
+
+    export ${__pipe_arg_key}="\"${__pipe_arg_val}\""
+    local __pipe_arg_key_list+=(${__pipe_arg_key})
+  done  
+  __runnerPrepare "$@"
+  if ! [ "$?" -eq 1 ]; then
+    echR "fail on call __runnerPrepare"
+    return 0
+  fi
+  echM "Pipelines"
+  echB "  Args:"
+  for __key in "${__pipe_arg_key_list[@]}"
+  do
+    echC "    - ${__key}: ${COLOR_YELLOW_B}${!__key}"
+  done
+
+  if [[ ${PIPELINE_ENVIRONMENT} == "" ]]; then
+    echR "Invalid --pipeline-environment"
+    return 0
+  fi
+ 
+  if [[ ${PIPELINE_NAME} == "" ]]; then
+    echR "Invalid --pipeline-name"
+    return 0
+  fi
+
+  if [[ ${PIPELINE_ENV_FILE} != "" ]]; then
+    if ! [[ -f ${PIPELINE_ENV_FILE} ]]; then
+      echR "File not found: --pipeline-env-file=${PIPELINE_ENV_FILE}"
+      return 0
+    fi
+  fi
+
+  return 1
+}
+
 # main system
 function runnerMain()
 {
   clearTerm
+  __private_check_is_pipeline "$@"
+  if [ "$?" -eq 1 ]; then
+    __private_pipeline_runner "$@"
+    if ! [ "$?" -eq 1 ]; then
+      echR "fail on call __private_check_is_pipeline"
+    fi
+    exit 0
+  fi
 
-  utilInitialize "$@"
-
-  if [[ ${PUBLIC_RUNNER_MODE} == test ]]; then
+  __private_check_is_test "$@"
+  if [ "$?" -eq 1 ]; then
     ./runner-test.sh
-    exit 0;
-  fi
-
-  clearTerm
-
-  dockerSwarmVerify
-  if ! [ "$?" -eq 1 ]; then
-    echR "  fail on calling dockerSwarmVerify"
     exit 0
   fi
 
-  if [[ ${__public_target} == "" ]]; then
-    selectorCustomer 1
-    if ! [ "$?" -eq 1 ]; then
-      echR "Invalid selectorCustomer"
-      echR "  fail on calling selectorCustomer"
-      exit 0
-    fi
-    export __public_target=${__selector}
-  fi
-
-  if [[ ${__public_environment} == "" ]]; then
-    selectorEnvironment 1
-    if ! [ "$?" -eq 1 ]; then
-      echR "  fail on calling selectorEnvironment"
-      exit 0
-    fi
-    export __public_environment=${__selector}
-  fi
-
-  utilPrepareInit "${__public_environment}" "${__public_target}"
-
+  __runnerPrepare "$@"
   if ! [ "$?" -eq 1 ]; then
-    echR "  fail on calling utilPrepareInit"
-    exit 0
+    echR "fail on call __runnerPrepare"
+    return 0
   fi
 
-  databasePrepare ${__public_environment} ${STACK_APPLICATIONS_DATA_DB_DIR}
-  if ! [ "$?" -eq 1 ]; then
-    echR "  fail on calling databasePrepare"
-    exit 0;
-  fi
-
-  scriptsPrepare ${__public_environment} ${STACK_APPLICATIONS_DATA_SCRIPT_DIR}
-  if ! [ "$?" -eq 1 ]; then
-    echR "  fail on calling scriptsPrepare"
-    exit 0;
-  fi
-
-  while :
-  do
-    __private_runnerMenu ${__public_environment} ${__public_target}
-  done
+  __private_runnerMenu ${__public_environment} ${__public_target}
 }
 
 runnerMain "$@"
